76b3367f07aa79ef7606884f65207486fbf5eadf
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-10-23 18:12:44 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Command } = require("commander");
 const program = new Command();
 
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-} = require("./contacts.js");
+const contactsOperations = require("./controllers/contacts");
+
+// const {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+// } = require("./contacts.js");
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -19,40 +21,42 @@ program.parse(process.argv);
 const argv = program.opts();
 
 // TODO: рефакторить
-(async ({ action, id, name, email, phone }) => {
-  try {
-    switch (action) {
-      case "list":
-        const contactsList = await listContacts();
-        console.table(contactsList);
-        break;
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contactsOperations.listContacts();
+      console.table(contactsList);
+      break;
 
-      case "get":
-        const contactById = await getContactById(id);
-        if (contactById) {
-          console.log("Contact found");
-          console.table(contactById);
-        } else {
-          throw new Error("contact not found");
-        }
-        break;
+    case "get":
+      const contactById = await contactsOperations.getContactById(id);
+      if (contactById) {
+        console.log("Contact found");
+        console.table(contactById);
+      } else {
+        throw new Error("contact not found");
+      }
+      break;
 
-      case "add":
-        const newContact = await addContact(name, email, phone);
-        console.log("Add new contact");
-        console.table(newContact);
-        break;
+    case "add":
+      const newContact = await contactsOperations.addContact(
+        name,
+        email,
+        phone,
+      );
+      console.log("Add new contact");
+      console.table(newContact);
+      break;
 
-      case "remove":
-        const contact = await removeContact(id);
-        console.log("Contact removed ");
-        console.table(contact);
-        break;
+    case "remove":
+      const contact = await contactsOperations.removeContact(id);
+      console.log("Contact removed ");
+      console.table(contact);
+      break;
 
-      default:
-        console.warn("\x1B[31m Unknown action type!");
-    }
-  } catch (error) {
-    throw error;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
-})(argv);
+};
+
+invokeAction(argv);
